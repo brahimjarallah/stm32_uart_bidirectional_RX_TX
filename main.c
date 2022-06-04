43a1a927cfd8660b4f459f8a1d79e75ad2e8ecd4
brahimjarallah <brahim.jarallah@gmail.com>
2022-06-04 18:56:46 +0100

    config usart tx & rx

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include <stm32f4xx.h>
 
-void USART_init(void){
+void USART2_init(void){
 	RCC->AHB1ENR |= 0x1; //enable port A
-	RCC->APB1ENR |= 0x200; // enable USART RX
+	RCC->APB1ENR |= 0x20000; // enable USART22
+	GPIOA->AFR[0] |= 0x7700; // enable alternate function register for USART2 RX & TX (PA2 & PA3 )
+	GPIOA->MODER |= 0xA0; // enable alternate function for PA2 & PA3
 
+	USART2-> BRR = 0x683; // BR = 9600 @ 16Mhz
+	USART2->CR1 |= 0xC; // 	enable UART TX & RX
+	USART2->CR1 |= 0x2000; // ENABLE UART MODULE
+}
+
+int USART2_write(int ch){
+	while(USART2->SR & 0x80){  // while TX buffer is empty
+		USART2->DR = (ch & 0xFF);
+		return ch;
+	}
+	}
+
+int USART2_read(void){
+	while(USART2->SR & 0x20){  // while RX buffer is empty
+		USART2->DR;
+	}
 }
