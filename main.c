cefbad2058fbd325dc977b452a93b696ad3367a9
Brahim Jarallah <brahim.jarallah@gmail.com>
2022-06-09 11:19:17 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stm32f4xx.h>
+#include <stdio.h>
 
-void USART2_init(void){
+void USART2_Init(void);
+int USART2_write(int ch);
+	
+int main(void){
+	int n;
+	char str[100];
+	printf("Hello there !\n");
+	fprintf(stdout,"test for stdout \r\n "); 
+	fprintf(stderr,"test for stderr \r\n "); 
+	while(1){
+		printf("how old are you?");
+		scanf("%d",&n);
+		printf("your age is: %d\r\n",n);
+		printf("Enter your first name:");
+		gets(str);
+		printf("I like your style: ");
+		puts(str);
+		printf("\r\n");
+	}
+}
+
+void USART2_Init(void){
 	RCC->AHB1ENR |= 0x1; //enable port A
 	RCC->APB1ENR |= 0x20000; // enable USART22
 	GPIOA->AFR[0] |= 0x7700; // enable alternate function register for USART2 RX & TX (PA2 & PA3 )
@@ -22,4 +44,24 @@ int USART2_read(void){
 	while(USART2->SR & 0x20){  // while RX buffer is empty
 		USART2->DR;
 	}
+}
+
+struct __FILE{int handle;};
+FILE __stdin = {0};
+FILE __stdout = {1}; 
+FILE __stderr = {2};
+
+int fgetc(FILE *f){
+	int c;
+	c =USART_read();
+	if(c == '\r'){
+		USART_write(c);
+		c='\n'; 
+	} 
+	USART2_write(c);
+	return c; 
+}
+
+int fputs(int c, FILE *f){
+	return USART2_write(c);  
 }
